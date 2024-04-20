1030174745593c4a0f8e753286837c1352d3fcd7
Jaeseob Hwang <jetsbee@gmail.com>
2024-04-20 05:47:09 +0000

    Implement draft list, publish, remove

--- blog/views.py
+++ blog/views.py
@@ -14,10 +14,22 @@ def post_list(request):
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
+    return render(request, "blog/post_draft_list.html", {"posts": posts})
+
+
 def post_detail(request, pk):
     # post = Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+
+    return redirect("post_detail", pk=post.pk)
 
 
 def post_new(request):
@@ -26,7 +38,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
 
             return redirect("post_detail", pk=post.pk)
@@ -36,6 +47,13 @@ def post_new(request):
     return render(request, "blog/post_edit.html", {"form": form})
 
 
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+
+    return redirect("post_list")
+
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -43,7 +61,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
 
             return redirect("post_detail", pk=post.pk)
